6f22c3e4cd8c2d5ce9994a663d4cf66ba684a561
James Sigurðarson <james@meskinn.is>
2013-06-07 16:57:16 +0000

    Fixed security vulnerabilities in REPL, added more extensibility for later implementations

--- client/CodeClient.js
+++ client/CodeClient.js
@@ -1,193 +1,188 @@
 function CodeClient (options) {
-	// this is really the JSHTML code client:
-	var CodeClientView = Backbone.View.extend({
-		className: "codeClient",
+    // this is really the JSHTML code client:
+    var CodeClientView = Backbone.View.extend({
+        className: "codeClient",
 
-		htmlEditorTemplate: _.template($("#jsCodeReplTemplate").html()),
+        htmlEditorTemplate: _.template($("#jsCodeReplTemplate").html()),
 
-		initialize: function (opts) {
-			var self = this;
+        initialize: function (opts) {
+            var self = this;
 
-			_.bindAll(this);
+            _.bindAll(this);
 
-			this.channelName = opts.room;
+            this.channelName = opts.room;
 
-			this.listen();
-			this.render();
+            this.listen();
+            this.render();
 
-			// debounce a function for auto-repling
-			this.doREPL = _.debounce(this._repl, 500);
+            // debounce a function for auto-repling
+            this.doREPL = _.debounce(this._repl, 500);
 
-			this.editors = {
-				"js": this.editorTemplates.SimpleJS($(".jsEditor", this.$el)[0]),
-				"html": this.editorTemplates.SimpleHTML($(".htmlEditor", this.$el)[0])
-			};
+            this.editors = {
+                "js": this.editorTemplates.SimpleJS($(".jsEditor", this.$el)[0]),
+                "html": this.editorTemplates.SimpleHTML($(".htmlEditor", this.$el)[0])
+            };
 
-			var syncedEditor = new SyncedEditor();
-			
+            var syncedEditor = new SyncedEditor();
+            
 
-			this.syncedJs = new syncedEditor({
-				room: this.channelName,
-				subchannel: "js",
-				editor: this.editors["js"]
-			});
-			this.syncedHtml = new syncedEditor({
-				room: this.channelName,
-				subchannel: "html",
-				editor: this.editors["html"]
-			});
+            this.syncedJs = new syncedEditor({
+                room: this.channelName,
+                subchannel: "js",
+                editor: this.editors["js"]
+            });
+            this.syncedHtml = new syncedEditor({
+                room: this.channelName,
+                subchannel: "html",
+                editor: this.editors["html"]
+            });
 
-			this.attachEvents();
+            this.attachEvents();
 
-			// currently triggered when user selects another channel:
-			// perhaps should also be triggered when a user chooses a different tab
-			this.on("show", this.show);
-			this.on("hide", this.hide);
-		},
+            // currently triggered when user selects another channel:
+            // perhaps should also be triggered when a user chooses a different tab
+            this.on("show", this.show);
+            this.on("hide", this.hide);
+        },
 
-		hide: function () {
-			DEBUG && console.log("code_client:hide");
-			this.$el.hide();
-			this.syncedJs.trigger("hide");
-			this.syncedHtml.trigger("hide");
-			// turn off live-reloading if we hide this view
-			// don't want to execute potential DoS code that was inserted while we were away
-			if (this.$livereload_checkbox &&
-				this.$livereload_checkbox.is(":checked")) {
-				this.$livereload_checkbox.attr("checked", false);
-			}
-		},
+        hide: function () {
+            DEBUG && console.log("code_client:hide");
+            this.$el.hide();
+            this.syncedJs.trigger("hide");
+            this.syncedHtml.trigger("hide");
+            // turn off live-reloading if we hide this view
+            // don't want to execute potential DoS code that was inserted while we were away
+            if (this.$livereload_checkbox &&
+                this.$livereload_checkbox.is(":checked")) {
+                this.$livereload_checkbox.attr("checked", false);
+            }
+        },
 
-		show: function () {
-			DEBUG && console.log("code_client:show");
-			this.$el.show();
-			this.syncedJs.trigger("show");
-			this.syncedHtml.trigger("show");
-		},
+        show: function () {
+            DEBUG && console.log("code_client:show");
+            this.$el.show();
+            this.syncedJs.trigger("show");
+            this.syncedHtml.trigger("show");
+        },
 
-		kill: function () {
-			var self = this;
+        kill: function () {
+            var self = this;
 
-			console.log("killing CodeClientView", self.channelName);
+            console.log("killing CodeClientView", self.channelName);
 
-			this.syncedJs.kill();
-			this.syncedHtml.kill();
-		},
+            this.syncedJs.kill();
+            this.syncedHtml.kill();
+        },
 
-		livereload: function () {
-			// only automatically evaluate the REPL if the user has opted in
-			if (this.$livereload_checkbox && 
-				this.$livereload_checkbox.is(":checked")) {
-				this.doREPL();
-			}
-		},
+        livereload: function () {
+            // only automatically evaluate the REPL if the user has opted in
+            if (this.$livereload_checkbox && 
+                this.$livereload_checkbox.is(":checked")) {
+                this.doREPL();
+            }
+        },
 
-		attachEvents: function () {
-			var self = this;
-			this.listenTo(this.syncedJs, "eval", this.livereload);
-			this.listenTo(this.syncedHtml, "eval", this.livereload);
-			$("body").on("codeSectionActive", function () {
-				self.refresh();
-			});
-			this.$el.on("click", ".evaluate", function (ev) {
-				ev.preventDefault();
-				ev.stopPropagation();
-				self._repl(); // doesn't need to be the debounced version since the user is triggering it purposefully
-			});
-		},
+        attachEvents: function () {
+            var self = this;
+            this.listenTo(this.syncedJs, "eval", this.livereload);
+            this.listenTo(this.syncedHtml, "eval", this.livereload);
+            $("body").on("codeSectionActive", function () {
+                self.refresh();
+            });
+            this.$el.on("click", ".evaluate", function (ev) {
+                ev.preventDefault();
+                ev.stopPropagation();
+                self._repl(); // doesn't need to be the debounced version since the user is triggering it purposefully
+            });
+        },
 
-		refresh: function () {
-			_.each(this.editors, function (editor) {
-				editor.refresh();
-			});
-		},
+        refresh: function () {
+            _.each(this.editors, function (editor) {
+                editor.refresh();
+            });
+        },
 
-		evaluateJS: function (userJs) {
-			// var iframe = document.getElementById("repl-frame").contentWindow;
-			var iframe = $("iframe.jsIframe", this.$el)[0];
+        evaluate: function (code) {
+            // var iframe = document.getElementById("repl-frame").contentWindow;
+            var $repl = $(".jsREPL .output", this.$el);
+            if (typeof code === "undefined") {
+                $repl.text("");
+                return;
+            }
+            var iframe = $("iframe.jsIframe", this.$el)[0];
+            //Register an event to run only once
+            
+            $(window).one('message', function(e){
+                var result;
+                try {
+                    result = JSON.parse(decodeURI(e.originalEvent.data)).result;
+                } catch (e) {
+                    result = e.toString();
+                }
+                if (_.isObject(result)) {
+                    result = JSON.stringify(result);
+                }
+                else if (result === undefined) {
+                    result = "undefined";
+                }
+                else {
+                    result = result.toString();
+                }
+                $repl.text(result);
+            });
+            //Send the message
+            iframe.contentWindow.postMessage(encodeURI(JSON.stringify({
+                type: 'js',
+                code: code
+            })),"*");
+            /*
+            iframe.contentWindow.postMessage('Hello',"*");*/
+        },
 
-			var wrapped_script = "(function(){ "; // execute in a closure
-				wrapped_script+= "return (function(window,$,_,alert,undefined) {"; // don't allow user to override things
-				wrapped_script+= userJs;
-				wrapped_script+= "})(window,$,_, function () { return arguments; });";
-				wrapped_script+= "})();";
+        _repl: function () {
 
-			if (userJs !== "") {
-				var result;
-				try {
-					result = iframe.contentWindow.eval(wrapped_script);
-					if (_.isObject(result)) {
-						result = JSON.stringify(result);
-					} 
-					else if (result === undefined) {
-						result = "undefined";
-					}
-					else {
-						result = result.toString();
-					}
-				} catch (e) {
-					result = e.toString();
-				}
-				$(".jsREPL .output", this.$el).text(result);
-			} else {
-				$(".jsREPL .output", this.$el).text("");
-			}
+            code = _.object(_.map(this.editors, function (editor,key) {
+                return [key,editor.getValue()];
+            }));
+            console.log(code);
+            // do HTML first so it's available to the user JS:
+            //this.evaluateHTML(html);
+            //this.evaluateJS(js);
+            this.evaluate(code);
+        },
 
-		},
+        editorTemplates: {
+            SimpleJS: function (domEl) {
+                return CodeMirror.fromTextArea(domEl, {
+                    lineNumbers: true,
+                    mode: "text/javascript",
+                    theme: "monokai",
+                    matchBrackets: true,
+                    highlightActiveLine: true,
+                    continueComments: "Enter"
+                });
+            },
+            SimpleHTML: function (domEl) {
+                return CodeMirror.fromTextArea(domEl, {
+                    lineNumbers: true,
+                    mode: "text/html",
+                    theme: "monokai"
+                });
+            }
+        },
 
-		evaluateHTML: function (userHtml) {
-			// var iframe = document.getElementById("repl-frame").contentWindow;
+        listen: function () {
 
-			// write the HTML
-			var doc = $("iframe.jsIframe", this.$el)[0].contentDocument;
-				doc.open();
-				doc.write(userHtml);
-				doc.close();
+        },
 
-		},
+        render: function () {
+            this.$el.html(this.htmlEditorTemplate());
+            this.$el.attr("data-channel", this.channelName);
 
-		_repl: function () {
+            this.$livereload_checkbox = this.$el.find("input.livereload");
+        },
 
-			var html = this.editors["html"].getValue();
-			var js = this.editors["js"].getValue();
+    });
 
-			// do HTML first so it's available to the user JS:
-			this.evaluateHTML(html);
-			this.evaluateJS(js);
-		},
-
-		editorTemplates: {
-			SimpleJS: function (domEl) {
-				return CodeMirror.fromTextArea(domEl, {
-					lineNumbers: true,
-					mode: "text/javascript",
-					theme: "monokai",
-					matchBrackets: true,
-					highlightActiveLine: true,
-					continueComments: "Enter"
-				});
-			},
-			SimpleHTML: function (domEl) {
-				return CodeMirror.fromTextArea(domEl, {
-					lineNumbers: true,
-					mode: "text/html",
-					theme: "monokai"
-				});
-			}
-		},
-
-		listen: function () {
-
-		},
-
-		render: function () {
-			this.$el.html(this.htmlEditorTemplate());
-			this.$el.attr("data-channel", this.channelName);
-
-			this.$livereload_checkbox = this.$el.find("input.livereload");
-		},
-
-	});
-
-	return CodeClientView; // todo: return a different view for different top-level options
+    return CodeClientView; // todo: return a different view for different top-level options
 }
