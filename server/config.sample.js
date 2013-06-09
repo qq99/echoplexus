2bec7db7028fea3b82bf80378323ee29841970b6
Cory Preus <cory@softwareforgood.com>
2013-06-09 14:42:18 -0500

    Initial SSL support.

--- server/config.sample.js
+++ server/config.sample.js
@@ -8,6 +8,10 @@
 			PORT: 8080,
 			USE_PORT_IN_URL: true,
 		},
+		ssl: {
+			PRIVATE_KEY: '/path/to/server.key',
+			CERTIFICATE: '/path/to/certificate.crt'
+		},
 		features: {
 			SERVER_NICK: 'Server',
 			phantomjs_screenshot: false, // http://www.youtube.com/watch?feature=player_detailpage&v=k3-zaTr6OUo#t=23s
