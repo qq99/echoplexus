3505eb29c23f8d4b63995dbd954cbba291db9d45
chemicalfire <okaychemicals@gmail.com>
2014-01-21 21:58:53 +0100

    Update client.js
    Display a basic d20 roll.

--- client/client.js
+++ client/client.js
@@ -348,6 +348,8 @@
 				});
 			} else if (body.match(REGEXES.commands.help)) {
 				socket.emit('help:' + room);
+			} else if (body.match(REGEXES.commands.roll)) {
+				socket.emit('chat:' + room, "rolled a" + Math.floor(Math.random()*21));
 			} else if (body.match(REGEXES.commands.failed_command)) { // match all
 				// NOOP
 			} else { // send it out to the world!
